cb728d591daeded3e6cf3ccf33f23b91ed1a0250
Brian Mattern <rephorm@rephorm.com>
2012-12-12 16:09:02 -0800

    WIP overly complex multiple type write/read

--- src/FortranIO.jl
+++ src/FortranIO.jl
@@ -10,7 +10,7 @@ function read_record{T}(io::IO, ::Type{T})
 
     # read in array
     arr = Array(T, div(nb,sizeof(T)))
-    arr = read(io, arr) # XXX StringIO doesn't fill the array in place!
+    arr = read(io, arr) # XXX IOString doesn't fill the array in place!
 
     nb_check = read(io, Int32)
     (nb == nb_check) || error("Invalid data $nb != $nb_check.")
@@ -18,11 +18,45 @@ function read_record{T}(io::IO, ::Type{T})
     arr
 end
 
-function write_record{T}(io::IO, a::Array{T})
-    n = int32(numel(a) * sizeof(T))
-    write(io, n)
-    write(io, a)
-    write(io, n)
+function read_record(io::IO, arrays::Array...)
+    nb = read(io, Int32)
+    println("Read size: $nb")
+
+    ret = Array[]
+    # read in arrays
+    for arr in arrays
+        arr = read(io, arr) # this is needed until IOString is fixed
+        push(ret, arr)
+    end
+
+    nb_check = read(io, Int32)
+    (nb == nb_check) || error("Invalid data $nb != $nb_check.")
+
+    ret
+end
+
+function write_record(io::IO, arrays::Array...)
+    # determine record size
+    nb = 0 
+    for arr in arrays
+        nb += numel(arr) * sizeof(eltype(arr))
+    end
+    nb = int32(nb)
+ 
+    # write record
+    write(io, nb)
+    for arr in arrays
+        println("Write $(arr)")
+        write(io, arr)
+    end
+    write(io, nb)
+end
+
+function write_record(io::IO, items...)
+    # first convert any non-array items to arrays
+    items = map(x -> isa(x,AbstractArray) ? x : [x], items)
+    println("Items: $(items)")
+    write_record(io, items...)
 end
 
 function write_record{T}(io::IO, v::T)
